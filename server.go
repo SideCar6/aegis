37eaf30ebaf2b9b3529d0bf422db399716c57333
Merkes <matt.merkes@gmail.com>
2014-09-13 23:05:45 -0700

    Remove Martini and implement http by hand

--- server.go
+++ server.go
@@ -3,9 +3,16 @@ package main
 import (
   "log"
   "net/http"
+  "strings"
+  "net/url"
+  "fmt"
 
   "github.com/googollee/go-socket.io"
+  "github.com/SideCar6/aegis/aegis_redis"
 )
+
+var chttp = http.NewServeMux()
+const api_url string = "/api/v1"
 
 func main() {
   server, err := socketio.NewServer(nil)
@@ -30,9 +37,60 @@ func main() {
   })
 
   http.Handle("/socket.io/", server)
-  http.Handle("/", http.FileServer(http.Dir("./public")))
 
+  chttp.Handle("/", http.FileServer(http.Dir("./public")))
 
-  log.Println("Serving at localhost:3000...")
+  http.HandleFunc("/", HomeHandler)
+
+  http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+    w.WriteHeader(200)
+    w.Write([]byte("Hello"))
+  })
+
+  http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+    w.WriteHeader(200)
+    w.Write([]byte("Test"))
+  })
+
+  http.HandleFunc(api_url + "/keys/", func(w http.ResponseWriter, r *http.Request) {
+    keys := aegis_redis.GetKeys()
+    w.WriteHeader(200)
+    w.Write(keys.ToJSON())
+  })
+
+  http.HandleFunc(api_url + "/stats", func(w http.ResponseWriter, r *http.Request) {
+    log.Println(r.Method)
+    switch r.Method {
+      case "GET": getStats(w, r)
+      case "POST": postStats(w, r)
+    }
+  })
+
   log.Fatal(http.ListenAndServe(":3000", nil))
+}
+
+func getStats(w http.ResponseWriter, r *http.Request) {
+  qs := r.URL.Query()
+  key, _ := url.QueryUnescape(qs.Get("key"))
+  stats := aegis_redis.GetList(key, 0, -1)
+  w.WriteHeader(200)
+  w.Write(stats.ToJSON())
+}
+
+func postStats(w http.ResponseWriter, r *http.Request) {
+  r.ParseForm()
+  body := r.Form["value"][0]
+  key := r.Form["key"][0]
+  fmt.Println(body, key)
+
+  aegis_redis.SetKey(key, body)
+  w.WriteHeader(201)
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+  if strings.Contains(r.URL.Path, ".") {
+    chttp.ServeHTTP(w, r)
+  } else {
+    http.ServeFile(w, r, "./public/index.html")
+  }
 }
